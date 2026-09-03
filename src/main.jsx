faacdd84977b40fee6b43337aac23642041905aa
Nehuelin <nehuelmarcos2005@gmail.com>
2026-09-03 16:44:49 -0300

    prevent random url insertion

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+if (window.location.pathname !== '/') {
+  window.history.replaceState(
+    null,
+    '',
+    `/${window.location.search}${window.location.hash}`,
+  )
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
